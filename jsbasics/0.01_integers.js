128f0eb28fb834c5846b1396ef8ba2a52dda8d8b
Turner-Tobie <turner.tobie@yahoo.com>
2016-09-21 10:30:11 -0400

    added store project and JavaScript files

---
+++ jsbasics/0.01_integers.js
@@ -0,0 +1,57 @@
+
+// var num = 7;
+// var newNum = 5; //this is camel casing
+// var newer_num = 10; //this is snake casing
+
+// //console.log() prints to the console
+
+// //operations with console.log
+// console.log((num * newer_num + newNum - newer_num)/newNum);
+
+// //if var is in all caps it is used as a constant
+
+// //Modulus Operator
+
+
+// //TODO- make a modulus that returns zero and another that returns a remainder
+// console.log(25 % 5);
+// console.log(25 % 6);
+
+// console.log(10 % 5); //returns zero
+// console.log(4 % 3);
+// console.log(8 % 3);
+
+// numOp = 17 *5;
+// console.log(numOp);
+
+// var a = 6;
+// var b = 10;
+// console.log (a + b * b);
+
+// var c = a + b;
+
+// console.log (c * b);
+
+var newNum = 10;
+newNum = newNum + 1;
+console.log (newNum);
+
+newNum++;
+console.log(newNum);
+
+newNum= newNum - 1;
+console.log(newNum);
+newNum--;
+console.log(newNum);
+
+newNum = newNum + 10;
+console.log(newNum);
+
+newNum += 10; //shortcut for writing newNum = newNum - 10
+console.log(newNum);
+
+newNum = newNum - 10;
+console.log(newNum);
+
+newNum -= 10;
+console.log(newNum);
