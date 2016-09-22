fc2c788100aa44001cba49b8ac2883291e37d52a
Turner-Tobie <turner.tobie@yahoo.com>
2016-09-22 11:25:33 -0400

    theDOM

---
+++ jsbasics/1.0_theDOM.js
@@ -0,0 +1,30 @@
+// document.onload(alert("HEY HEY HEY!!!"));
+// alert("HEY HEY HEY!!!");
+// console.log("Look at me, we are working together!")
+
+// var	hobbieslist = document.getElementById("hobby-list");
+// var hobbies = document.getElementsByClassName("hobby");
+// console.log(hobbieslist);
+// console.log(hobbies);//an array
+// console.log(hobbies[0].innerHTML);
+// console.log(hobbies[1].innerHTML);
+// console.log(hobbies[2].innerHTML);
+// console.log(hobbies[3].innerHTML);
+
+function worstSong() {
+	document.getElementById("worst").innerHTML="Anything by Nirvana!";
+}
+
+worstSong();
+
+function bestSong() {
+	document.getElementById("best").innerHTML="Bohemian Rhapsody!";
+}
+
+bestSong();
+
+function favoriteSong() {
+	document.getElementById("favorite").innerHTML="The song that never ends";
+}
+
+favoriteSong();
